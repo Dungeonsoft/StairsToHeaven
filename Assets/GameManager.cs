9afa8ee624b518647b98698d9634e822529c0b3d
YG Ryu <nauryu@gmail.com>
2015-03-20 18:46:15 +0900

    First Upload
    깃허브 게시

---
+++ Assets/GameManager.cs
@@ -0,0 +1,602 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    //public static bool isOnStep;
+
+
+    public enum status
+    {
+        idle,
+        jumpLeft,
+        jumpRight
+    }
+
+
+    public GameObject ReStartWindow;
+    public GameObject player;
+    public UISlider lifeGage;
+    public UISlider feverGage;
+    public UILabel stepView;
+    public bool baseTimeLife;
+    public float jumpSpeed = 20;
+    float tempJumpSpeed;
+    status pStatus;
+  
+    Vector3[] scaleV;
+    GameObject[] playerPrefab;
+    GameObject stepPrefab;
+    GameObject[] buildingPrefab;
+
+    int playerNum;
+    bool isPlayerDrop;
+    float chFirstPosX;
+
+    Vector3 camPos;
+    Vector3 plPos;
+    Vector3 nowPos;
+    Vector3 newPos;
+    Vector3 oriCamPos;
+
+    int jumpCount;
+    int addVal;
+    int frameCount;
+
+    bool isDaed;
+    bool isPlayerDead;
+
+    float camPosX;
+    float camPosY;
+    float playerPosY;
+    float oriJumpSpeed;
+    float collapseInter;
+    float StepMakePosX;
+    float StepMakePosY;
+    float spendTime;
+
+    public Transform instanceStepsParent;
+    Transform GO;
+    int stepChCount;
+    
+    public Transform stairs;
+    Transform stairChild;
+
+    int stairChCount;
+
+    public Transform instanceBuildingParent;
+    public Transform buildings;
+
+    public float jumpX;
+    public float jumpY;
+
+    Transform nextStepT;
+    GameObject objInstance;
+
+    public GameObject[] moveBtn;
+
+    bool isFever;
+    void Awake()
+    {
+        int TempI = 0;
+        GameObject[] PrefabTemp;
+
+        oriCamPos = Camera.main.transform.localPosition;
+        ReStartWindow.SetActive(false);
+        oriJumpSpeed = jumpSpeed;
+
+        //계단 프리팹 로드//
+        stepPrefab = Resources.Load("StepBox00") as GameObject;
+        //계단 인스턴스 풀 생성//
+        ObjPools(stepPrefab, "Step", 30, instanceStepsParent);
+
+        //빌딩 프리팹 로드//
+        buildingPrefab = new GameObject[10];
+        TempI = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log("BuildingAA" + i.ToString("D3"));
+            if (Resources.Load("BG/BuildingAA" + i.ToString("D3")) == null) { Debug.Log("I값 : " + i); TempI = i; break; }
+            buildingPrefab[i] = Resources.Load("BG/BuildingAA" + i.ToString("D3")) as GameObject;
+        }
+        PrefabTemp = buildingPrefab;
+        buildingPrefab = new GameObject[TempI];
+        for (int j = 0; j < TempI; j++)
+        {
+            buildingPrefab[j] = PrefabTemp[j];
+        }
+        //빌딩 인스턴스 풀 생성//
+        for (int i = 0; i < buildingPrefab.Length; i++)
+        {
+            ObjPools(buildingPrefab[i], "BuildingAA", 30, instanceBuildingParent);
+        }
+        
+        
+        
+        //캐릭터 프리팹 로드//
+        playerPrefab = new GameObject[10];
+        TempI = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            if (Resources.Load("Characters/Character" + i.ToString("D3")) == null) { Debug.Log("I값 : " + i); TempI = i; break; }
+            playerPrefab[i] = Resources.Load("Characters/Character" + i.ToString("D3")) as GameObject;
+        }
+        PrefabTemp = playerPrefab;
+        playerPrefab = new GameObject[TempI];
+        for (int j = 0; j < TempI; j++)
+        {
+            playerPrefab[j] = PrefabTemp[j];
+        }
+
+        isDaed = true;
+        GetComponent<PopupController>().AddPopWin(ReStartWindow);
+        //Activate();
+    }
+
+    void ObjPools(GameObject obj, string newName, int count, Transform parentT)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            objInstance = Instantiate(obj) as GameObject;
+            objInstance.transform.parent = parentT;
+            objInstance.name = newName + i.ToString("D5");
+            objInstance.SetActive(false);
+        }
+    }
+
+    void Activate()
+    {
+        #region//기본값 세팅//
+        isPlayerDrop = false;
+        frameCount = 0;
+        jumpCount = 0;
+        isDaed = false;
+        isPlayerDead = false;
+
+        spendTime = 0;
+
+        StepMakePosX = 0;
+        StepMakePosY = 0;
+
+        //최초상태 세팅//
+        pStatus = status.idle;
+        lifeGage.value = 1f;
+        feverGage.value = 0f;
+        stepView.text = "0";
+        isFever = false;
+        //카메라 최초 위치
+        Camera.main.transform.localPosition = oriCamPos;
+        
+        #endregion//기본값 세팅//
+
+        jumpSpeed = oriJumpSpeed / 100;
+        collapseInter = jumpSpeed / 50;
+
+        //계단 인스턴스 화면 배치//
+        StepMaker();
+
+        //빌딩 인스턴스 화면 배치//
+        BuildingMaker();
+
+
+        //캐릭터 프리팹 화면에 배치//
+        playerNum = Random.Range(0, playerPrefab.Length);
+        GameObject selChar = playerPrefab[playerNum];
+        
+        player = Instantiate(selChar) as GameObject;
+
+        player.transform.parent = GameObject.Find("GameRoot").transform;
+        player.transform.localPosition = new Vector3(chFirstPosX, 0, 0);
+        player.name = "Player";
+
+    }
+
+    void Update()
+    {
+        if (isDaed == true) return;
+
+        camPos = Camera.main.transform.localPosition;
+        plPos = player.transform.localPosition;
+
+        camPosY = camPos.y;
+        playerPosY = plPos.y;
+        if (camPosY > (playerPosY + 3.8f)) isPlayerDrop = true;
+
+        #region //계단을 밟지 못했을 경우// 라이프 게이지가 다 닳아 없어졌을때//화면 밖으로 나갔을 때//
+        if (isPlayerDrop == true)
+        {
+            player.transform.localPosition += new Vector3(0, -0.1f, 0);
+            if (isPlayerDead == false)
+            {
+                isPlayerDead = true;
+                StartCoroutine(PlayerDead());
+            }
+            return;
+        }
+        #endregion //계단을 밟지 못했을 경우//
+
+        #region //키보드 인풋//
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) JumpLeft();
+        if (Input.GetKeyDown(KeyCode.RightArrow)) JumpRight();
+        #endregion //키보드 인풋//
+
+        #region //카메라 위치 잡아줌//
+        //캠 Y위치 교정//
+        camPosY = camPos.y;
+        if (camPosY - 1 <= plPos.y)
+        {
+            //Debug.Log("카메라 교정");
+            camPosY = Mathf.Lerp(camPos.y, plPos.y + 1, 0.1f);
+            //피버게이지 채움//
+            if (jumpCount > 0 && isFever == false)
+            {
+                if (feverGage.value < 1)
+                {
+                    //피버게이지 채움//
+                    //Debug.Log("피버게이지 채움");
+                    feverGage.value += 0.0006f;
+                    //feverGage.value += 0.003f;
+                }
+                else
+                {
+                    isFever = true;
+                    tempJumpSpeed = jumpSpeed;
+                    jumpSpeed = 1;
+                    moveBtn[0].SetActive(false);
+                    moveBtn[1].SetActive(false);
+                    moveBtn[2].SetActive(true);
+                }
+            }
+        }
+        //피버게이지가 다 찾으니 피버가 발동하면서 피버게이지 줄임.
+        if (isFever == true)
+        {
+            feverGage.value -= Time.deltaTime * 0.1f;
+            if (feverGage.value <= 0)
+            {
+                isFever = false;
+                feverGage.value = 0;
+
+                moveBtn[0].SetActive(true);
+                moveBtn[1].SetActive(true);
+                moveBtn[2].SetActive(false);
+
+                jumpSpeed = tempJumpSpeed;
+            }
+        }
+        else
+        {
+            #region //상단 생명력 표시 실시간 변화//
+            switch (baseTimeLife)
+            {
+                case true:
+                    //시간기준으로 생명력 단축//
+                    if (spendTime < 200) spendTime += Time.deltaTime * 2;
+                    //Debug.Log("Spend Time :: " + spendTime);
+                    lifeGage.value -= Time.deltaTime * (0.3f + spendTime * 0.001f);
+                    break;
+                //시간기준으로 생명력 단축//
+
+                case false:
+                    //오른 계단 기준으로 생명력 단축//
+                    if (jumpCount > 0)
+                    {
+                        if (jumpCount < 200) addVal = jumpCount; else addVal = 200;
+                        lifeGage.value -= Time.deltaTime * (0.3f + addVal * 0.001f);
+                    }
+                    break;
+                //오른 계단 기준으로 생명력 단축//
+            }
+            if (lifeGage.value <= 0)
+                isPlayerDrop = true;
+            //오른 계단 기준으로 생명력 단축//
+            #endregion //상단 생명력 표시 실시간 변화//
+        }
+
+
+        //캠이 서서히 위로 올라가서 캐릭터가 화면 밑으로 내려가도록 Y값을 보정//
+        camPosY += 0.01f;
+
+        camPosX = Mathf.Lerp(camPos.x, plPos.x, 0.2f);
+        Camera.main.transform.localPosition = new Vector3(camPosX, camPosY, camPos.z);
+        #endregion //카메라 위치 잡아줌//
+
+        #region //플레이어 상태에 따른 변화-실질적으로 점프 체크를 위해 필요//
+        switch (pStatus)
+        {
+            case status.idle:
+                player.transform.GetChild(0).animation.Play("idle");
+                break;
+
+            case status.jumpLeft:
+                frameCount++;
+                //Debug.Log("Frame Count = " + frameCount);
+                player.transform.localPosition = Vector3.Lerp(nowPos, newPos, jumpSpeed * frameCount);
+                //if (jumpSpeed * frameCount > .1f) Debug.Break();
+                player.transform.localRotation = Quaternion.Euler(0, 45, 0);
+                //player.transform.GetChild(0).animation.Play("jump");
+                IsJumpComplete();
+                break;
+
+            case status.jumpRight:
+                frameCount++;
+                //Debug.Log("Frame Count = " + frameCount);
+                player.transform.localPosition = Vector3.Lerp(nowPos, newPos, jumpSpeed * frameCount);
+                //if (jumpSpeed * frameCount > .1f) Debug.Break();
+                player.transform.localRotation = Quaternion.Euler(0, -45, 0);
+                //player.transform.GetChild(0).animation.Play("jump");
+                IsJumpComplete();
+                break;
+        }
+        #endregion //플레이어 상태에 따른 변화-실질적으로 점프 체크를 위해 필요//
+
+
+    }
+
+    void IsJumpComplete()
+    {
+        float nowPosX = player.transform.localPosition.x;
+        float newPosX = newPos.x;
+        float inter2Pos = 0;
+
+        switch (pStatus)
+        {
+            case status.jumpLeft:
+                inter2Pos = nowPosX - newPosX;
+                break;
+
+            case status.jumpRight:
+                inter2Pos = newPosX - nowPosX;
+                break;
+        }
+
+        //계단을 밟았는지 여부 검사//
+        if (inter2Pos < collapseInter)
+        {
+            Debug.Log("jumpCount :: " + jumpCount +" :: 밟음 여부 검사");
+            player.transform.localPosition = newPos;
+            pStatus = status.idle;
+            frameCount = 0;
+            nextStepT = stairs.FindChild("Step" + jumpCount.ToString("D5")).transform;
+            nextStepT.localScale = Vector3.one;
+            if (newPos.x != nextStepT.localPosition.x)
+            {
+                isPlayerDrop = true;
+            }
+        }
+    }
+
+
+    void StepMaker()
+    {
+        stepChCount = instanceStepsParent.childCount;
+
+        StepMakePosX = Mathf.Floor(StepMakePosX);
+        chFirstPosX = StepMakePosX;
+        //Debug.Log("posX : " + StepMakePosX);
+
+        for (int i = 0; i < 15; i++)
+        {
+            //Debug.Log("계단생성 함?");
+            //GO = Instantiate(prefabO) as GameObject;
+            for (int j = 0; j < stepChCount; j++)
+            {
+                GO = instanceStepsParent.GetChild(j);
+                if (GO.gameObject.activeSelf == false)
+                    GO.gameObject.SetActive(true);
+                GO.localScale = new Vector3(1, 1, 0.1f);
+                break;
+            }
+
+            GO.parent = stairs;
+            GO.localPosition = new Vector3(StepMakePosX, StepMakePosY, 0.5f);
+            GO.name = "Step" + i.ToString("D5");
+
+            StepNextPos();
+        }
+        stairs.GetChild(0).localScale = Vector3.zero;
+    }
+    void StepRelocater(int StepNum)
+    {
+        if (jumpCount < 8) return;
+        GO = stairs.GetChild(0);
+        GO.parent = instanceStepsParent;
+        GO.parent = stairs;
+        GO.localPosition = new Vector3(StepMakePosX, StepMakePosY, 0.5f);
+        GO.localScale = Vector3.zero;
+        GO.name = "Step" + StepNum.ToString("D5");
+
+        StartCoroutine(ScaleUpStep(GO.transform));
+
+        StepNextPos();
+    }
+
+    void StepNextPos()
+    {
+        int leftRight;
+        StepMakePosY += jumpY;
+
+        if (StepMakePosX <= -10)
+        {
+            StepMakePosX += jumpX; 
+            return;
+        }
+        if (StepMakePosX >= 10)
+        {
+            StepMakePosX += -jumpX; 
+            return;
+        }
+
+        leftRight = Random.Range(0, 100);
+
+        if (leftRight < 50)
+            StepMakePosX += -jumpX;
+        else
+            StepMakePosX += jumpX;
+    }
+
+    void BuildingMaker()
+    {
+        Transform bgObj;
+        for (int i = 0; i < 6; i++)
+        {
+            bgObj = instanceBuildingParent.GetChild(0);
+            bgObj.parent = buildings;
+            bgObj.gameObject.SetActive(true);
+            bgObj.localPosition = new Vector3(0, i * 3, 0.5f);
+        }
+    }
+
+    void BuildingRelocater()
+    {
+        if (camPos.y > buildings.GetChild(0).localPosition.y + 9)
+        {
+            GO = buildings.GetChild(0);
+            GO.parent = instanceBuildingParent;
+            GO.parent = buildings;
+            GO.localPosition += new Vector3(0, 18, 0);
+        }
+    }
+
+    void JumpDefault(int leftRight =1)
+    {
+        JumpCounter();
+        player.transform.GetChild(0).animation.Play("jump");
+        nowPos = player.transform.localPosition;
+        newPos = nowPos + new Vector3(leftRight*jumpX, jumpY, 0);
+
+    }
+
+    public void JumpLeft()
+    {
+        if (pStatus == status.jumpLeft || pStatus == status.jumpRight || isPlayerDrop == true || player == null) return;
+
+        pStatus = status.jumpLeft;
+        JumpDefault(-1);
+    }
+
+    public void JumpRight()
+    {
+        if (pStatus == status.jumpLeft || pStatus == status.jumpRight || isPlayerDrop == true || player == null) return;
+
+        pStatus = status.jumpRight;
+        JumpDefault(1);
+    }
+
+    int centerLR;
+    public void JumpCenter()
+    {
+        if (pStatus == status.jumpLeft || pStatus == status.jumpRight || isPlayerDrop == true || player == null) return;
+
+        JumpCounter();
+        player.transform.GetChild(0).animation.Play("jump");
+        nowPos = player.transform.localPosition;
+
+        nextStepT = stairs.FindChild("Step" + (jumpCount).ToString("D5")).transform;
+        if (nextStepT.localPosition.x > nowPos.x)
+        { pStatus = status.jumpRight; centerLR = 1; }
+        else
+        { pStatus = status.jumpLeft; centerLR = -1; }
+
+        newPos = nowPos + new Vector3(centerLR * jumpX, jumpY, 0);
+
+        Debug.Log("pStatus :::: "+pStatus.ToString());
+        Debug.Log("newPos :::: " + newPos);
+
+    }
+
+    //계단 자동 생성//
+    void JumpCounter()
+    {
+        jumpCount++;
+
+        stepView.text = jumpCount.ToString();
+        StartCoroutine(BubbleAction(stepView.transform));
+        GetComponent<AudioSource>().Play();
+
+        //생명 연장//
+        lifeGage.value += 0.1f;
+        if (lifeGage.value > 1) lifeGage.value = 1;
+
+
+        StepRelocater(jumpCount + 7);
+        //ExtendStepMaker(jumpCount+6);
+        //hideSteps();
+
+        BuildingRelocater();
+    }
+
+    IEnumerator ScaleUpStep(Transform step)
+    {
+        float val = 0;
+        while (val < 1)
+        {
+            step.localScale = new Vector3(val, val, val * 0.1f);
+            yield return null;
+            val += Time.deltaTime * 3;
+        }
+        step.localScale = new Vector3(1, 1, 0.1f);
+    }
+
+    IEnumerator PlayerDead()
+    {
+        yield return new WaitForSeconds(0.5f);
+        isDaed = true;
+
+        Destroy(GameObject.Find("Player"));
+
+        GetComponent<PopupController>().AddPopWin(ReStartWindow);
+    }
+
+    public void ReStart()
+    {
+        int child = stairs.childCount;
+        Transform cObj;
+        for (int i = 0; i < child; i++)
+        {
+            cObj = stairs.GetChild(0);
+            cObj.parent = instanceStepsParent;
+            cObj.gameObject.SetActive(false);
+        }
+
+        child = buildings.childCount;
+        for (int i = 0; i < child; i++)
+        {
+            cObj = buildings.GetChild(0);
+            cObj.parent = instanceBuildingParent;
+            cObj.gameObject.SetActive(false);
+        }
+
+        GetComponent<PopupController>().CloseWindow();
+        StartCoroutine(ReAct());
+    }
+
+    IEnumerator ReAct()
+    {
+        yield return null;
+        Activate();
+    }
+
+    IEnumerator BubbleAction(Transform obj)
+    {
+        scaleV = new Vector3[]{ new Vector3(0.7f, 0.7f, 1f), new Vector3(1.2f, 1.2f, 1), new Vector3(1f, 1f, 1f) };
+        
+        for (int i = 0; i < scaleV.Length - 1; i++)
+        {
+            float val = 0;
+            while (val < 1)
+            {
+                obj.localScale = Vector3.Lerp(scaleV[i], scaleV[i + 1], val);
+                //Debug.Log("Win.localScale::: :" + i + ": ::: " + Win.localScale);
+
+                val += Time.deltaTime * (10 + i);
+                yield return null;
+            }
+        }
+
+        obj.localScale = new Vector3(1, 1, 1);
+    }
+
+}
---
+++ Assets/ScreenSizeMatchingScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSizeMatchingScript : MonoBehaviour
+{
+    public enum OrienteScreen : byte { landscape, portrait };
+    public enum ArrangeScreen : byte { top, bottom, left, right };
+
+    int scrX;
+    int scrY;
+    float scrRatio;
+    public OrienteScreen oriScreen;
+    public ArrangeScreen arScreen;
+    //public bool isRight;
+    public int intervalPixel = 100;
+
+    int plusMinus;
+    float xRatio;
+    float yRatio;
+    float dftXVal;
+    float dftYVal;
+    float fixedRatio;
+    float xVal;
+    float yVal;
+    float zVal;
+
+    void Awake()
+    {
+        switch (arScreen)
+        {
+            case ArrangeScreen.top :
+            case ArrangeScreen.right :
+               plusMinus = 1;
+               break;
+
+            case ArrangeScreen.bottom:
+            case ArrangeScreen.left:
+                 plusMinus = -1;
+              break;
+        }
+
+
+
+        switch (oriScreen)
+        {
+            case OrienteScreen.portrait:
+                dftXVal = 720f;
+                dftYVal = 1280f;
+                break;
+
+            case OrienteScreen.landscape:
+                dftXVal = 1280f;
+                dftYVal = 720f;
+                break;
+        }
+
+
+        scrX = Screen.width;
+        scrY = Screen.height;
+        fixedRatio = (dftXVal/dftYVal) / ((float)scrX / scrY) ;
+        xRatio = scrX / dftXVal;
+        yRatio = scrY / dftYVal;
+        xVal = 0;
+        yVal = 0;
+        zVal = 0;
+
+        switch (arScreen)
+        {
+            case ArrangeScreen.top:
+            case ArrangeScreen.bottom:
+                yVal = plusMinus * (dftYVal / 2 - intervalPixel) * fixedRatio;
+                break;
+
+            case ArrangeScreen.left:
+            case ArrangeScreen.right:
+                xVal = plusMinus * (dftXVal / 2 - intervalPixel) * fixedRatio;
+                break;
+        }
+        transform.localPosition = new Vector3(xVal, yVal, zVal);
+    }
+}
